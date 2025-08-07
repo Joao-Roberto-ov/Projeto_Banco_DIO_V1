46d2074a591cccc2111f8cd3e1e60b8b3a1b1223
João Roberto <jrworks2002@gmail.com>
2025-08-07 18:00:51 -0300

    Criação do menu de entrada

--- Banco_v1.py
+++ Banco_v1.py
@@ -1,1 +1,10 @@
 while True:
+    opcao = int(input("""
+        ------ Boas Vindas ao Banco DIO ------
+        
+        [1] Depositar
+        [2] sacar
+        [3] Visualizar Extrato
+        [4] Sair
+        
+        Selecione a opção que deseja realizar: """))
