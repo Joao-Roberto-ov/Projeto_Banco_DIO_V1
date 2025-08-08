fccbee57b5577daa1d255281703dc7f2bd13ebed
João Roberto <jrworks2002@gmail.com>
2025-08-08 13:25:54 -0300

    Modificação e adição de algumas variáveis com pretenção de melhorias visuais, além da implementação de tratamento de erros para algumas funções.

--- Banco_v1.py
+++ Banco_v1.py
@@ -4,8 +4,9 @@ dinheiro_disponivel_conta = 0.0
 LIMITE_VALOR_SAQUE = 500
 saques_realizados = 0
 extrato = []
-
-menu = """
+nome_extrato = " EXTRATO "
+nome_modificado = nome_extrato.center(58,"=")
+menu = """\n
 ------ Boas Vindas ao Banco DIO ------
 
 [1] Depositar
@@ -18,7 +19,12 @@ Selecione a opção que deseja realizar: """
 def sacar():
     global dinheiro_disponivel_conta, LIMITE_VALOR_SAQUE, saques_realizados
 
-    valor_sacado = float(input('Digite o valor do saque: '))
+    try:
+        valor_sacado = float(input('Digite o valor do saque: '))
+    except ValueError:
+        print("Tentativa de saque falhou. Valor invalido!")
+        return
+
     if saques_realizados == 3:
         print("Limite de saques diários atingido, tente novamente amanha.")
 
@@ -31,41 +37,55 @@ def sacar():
     else:
         dinheiro_disponivel_conta -= valor_sacado
         print(f"Saldo disponível na conta: R${dinheiro_disponivel_conta:.2f}")
-        data_modificada = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        data_modificada = datetime.now().strftime("%d/%m/%Y às %H:%M:%S")
         extrato.append(f"Saque de R${valor_sacado:.2f} realizado em {data_modificada}")
         saques_realizados += 1
 
 def mostra_extrato():
-    for item in extrato:
-        print(f"{item}")
+
+    if not extrato:
+        print(f"\n{nome_modificado}")
+        print("Nenhuma movimentação realizada na conta.")
+    else:
+        print(f"\n{nome_modificado}")
+        for item in extrato:
+            print(f"{item}")
 
 def depositar():
     global dinheiro_disponivel_conta
-    valor_depositado = float(input("Digite a quantia a ser depositada: "))
+
+    try:
+        valor_depositado = float(input("Digite a quantia a ser depositada: "))
+    except ValueError:
+        print("Tentativa de depósito falhou. Valor invalido!")
+        return
 
     if valor_depositado > 0:
         dinheiro_disponivel_conta += valor_depositado
-        data_modificada = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        data_modificada = datetime.now().strftime("%d/%m/%Y às %H:%M:%S")
         extrato.append(f"Deposito de R${valor_depositado:.2f} realizado em {data_modificada} ")
         print(f"Saldo disponível na conta: R${dinheiro_disponivel_conta:.2f}")
 
     else:
-        print("Valor inválido para deposito.")
+        print("Valor inválido para depósito.")
 
 while True:
 
-    opcao = int(input(menu))
+    opcao = input(menu)
 
-    if opcao == 1:
+    if opcao == "1":
         depositar()
 
-    if opcao == 2:
+    elif opcao == "2":
         sacar()
 
-    if opcao == 3:
+    elif opcao == "3":
         mostra_extrato()
 
-    if opcao == 4:
+    elif opcao == "4":
         break
+
+    else:
+        print("Opção inválida. Tente novamente.")
 
 
